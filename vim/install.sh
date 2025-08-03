180fcb9ca54f88a7fc5bd6aa40fde69fd0fac603
ding.zheng <ding.zheng@foryouge.com.cn>
2025-08-03 19:23:18 +0800

    [vim] del: 删除.vim文件夹

--- vim/install.sh
+++ vim/install.sh
@@ -19,13 +19,23 @@ else
         sudo yum update -y vim
     fi
 fi
+# 检查 nodejs 是否安装
+if ! command -v node &> /dev/null; then
+    echo "nodejs 未安装，正在尝试安装..."
+    # Download and install nvm:
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+    # in lieu of restarting the shell
+    \. "$HOME/.nvm/nvm.sh"
+    # Download and install Node.js:
+    nvm install 22
+fi
 
 # 创建备份目录
 BACKUP_DIR="$HOME/.dingTools/vim"
 mkdir -p "$BACKUP_DIR"
 
 # 备份与 vim 相关的配置文件
-for file in "$HOME/.vimrc" "$HOME/.vim" "$HOME/.viminfo"; do
+for file in "$HOME/.vimrc" "$HOME/.viminfo"; do
     if [ -e "$file" ]; then
         mv "$file" "$BACKUP_DIR/"
         echo "已备份 $file 到 $BACKUP_DIR"
@@ -40,13 +50,13 @@ else
     echo "未找到当前目录下的 .vimrc 文件，跳过复制。"
 fi
 
-# 拷贝当前目录下的 .vim 到 ~/
-if [ -d ".vim" ]; then
-    cp -rf .vim "$HOME/.vim"
-    echo "已将 .vim 拷贝到 ~/"
-else
-    echo "未找到当前目录下的 .vim 文件夹，跳过复制。"
-fi
+# # 拷贝当前目录下的 .vim 到 ~/
+# if [ -d ".vim" ]; then
+#     cp -rf .vim "$HOME/.vim"
+#     echo "已将 .vim 拷贝到 ~/"
+# else
+#     echo "未找到当前目录下的 .vim 文件夹，跳过复制。"
+# fi
 
 echo "vim 配置完成。"
 
